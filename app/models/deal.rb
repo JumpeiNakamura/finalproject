d829f3a32ff43805c723e8d357a82aeac03bb4a9
JumpeiNakamura <jun.n1004@gmail.com>
2014-03-15 19:24:10 -0500

    added validation and callback

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -4,11 +4,18 @@ class Deal < ActiveRecord::Base
   belongs_to :products
   has_many :reviews
 
-  validates_numericality_of :original_price
-  validates_numericality_of :deal_price
+  validates_numericality_of :original_price, :message => "Original price is required"
+  validates_numericality_of :deal_price, :message => "Deal price is required"
   validates_inclusion_of :original_price, :in=>0..10000
   validates_inclusion_of :deal_price, :in=>0..10000
 
+  before_validation :ensure_original_price_has_a_value
 
+  protected
+  def ensure_original_price_has_a_value
+    if original_price.nil?
+      self.original_price = deal_price unless deal_price.blank?
+    end
+  end
 
 end
